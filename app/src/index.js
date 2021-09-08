77fe8c1660ff08063f028d37fc63c7961556035c
Leo Bonato <leo.bonato@protonmail.com>
2021-09-08 18:38:41 +0200

    updated frontend to use volcanocoin

--- app/src/index.js
+++ app/src/index.js
@@ -1,21 +1,21 @@
 import Web3 from "web3";
-import metaCoinArtifact from "../../build/contracts/MetaCoin.json";
+import volcanoCoinArtifact from "../../build/contracts/VolcanoCoin.json";
 
 const App = {
   web3: null,
   account: null,
-  meta: null,
+  volcano: null,
 
-  start: async function() {
+  start: async function () {
     const { web3 } = this;
 
     try {
       // get contract instance
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = metaCoinArtifact.networks[networkId];
-      this.meta = new web3.eth.Contract(
-        metaCoinArtifact.abi,
-        deployedNetwork.address,
+      const deployedNetwork = volcanoCoinArtifact.networks[networkId];
+      this.volcano = new web3.eth.Contract(
+        volcanoCoinArtifact.abi,
+        deployedNetwork.address
       );
 
       // get accounts
@@ -28,28 +28,28 @@ const App = {
     }
   },
 
-  refreshBalance: async function() {
-    const { getBalance } = this.meta.methods;
-    const balance = await getBalance(this.account).call();
+  refreshBalance: async function () {
+    const { balanceOf } = this.volcano.methods;
+    const balance = await balanceOf(this.account).call();
 
     const balanceElement = document.getElementsByClassName("balance")[0];
     balanceElement.innerHTML = balance;
   },
 
-  sendCoin: async function() {
+  sendCoin: async function () {
     const amount = parseInt(document.getElementById("amount").value);
     const receiver = document.getElementById("receiver").value;
 
     this.setStatus("Initiating transaction... (please wait)");
 
-    const { sendCoin } = this.meta.methods;
-    await sendCoin(receiver, amount).send({ from: this.account });
+    const { transfer } = this.volcano.methods;
+    await transfer(receiver, amount).send({ from: this.account });
 
     this.setStatus("Transaction complete!");
     this.refreshBalance();
   },
 
-  setStatus: function(message) {
+  setStatus: function (message) {
     const status = document.getElementById("status");
     status.innerHTML = message;
   },
@@ -57,18 +57,18 @@ const App = {
 
 window.App = App;
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
   if (window.ethereum) {
     // use MetaMask's provider
     App.web3 = new Web3(window.ethereum);
     window.ethereum.enable(); // get permission to access accounts
   } else {
     console.warn(
-      "No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live",
+      "No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live"
     );
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
     App.web3 = new Web3(
-      new Web3.providers.HttpProvider("http://127.0.0.1:8545"),
+      new Web3.providers.HttpProvider("http://127.0.0.1:8545")
     );
   }
 
